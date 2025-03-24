34c64d2ba75cd0eaf0a26c27c1e4004ac093f08b
Durga-velu <durgadevi134babu@gmail.com>
2025-03-24 09:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ class Teacher extends Person {
 
 const person = new Person("John", 25);
 console.log(person.name);
-Person.fullAge = 30;
-console.log(Person.age);
+person.fullAge = 30;
+console.log(person.age);
 
 const student = new Student("Alice", 22);
 student.study(); 
